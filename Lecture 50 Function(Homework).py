aabeec2ec16d4cfa22e105edacba1d8a29bba8a4
Booklove22u <alex.ps.phongsili1998@gmail.com>
2025-07-07 18:40:50 -0700

    Lecture 50 Function (Homwork)
    Homework Lecture 50

---
+++ Lecture 50 Function(Homework).py
@@ -0,0 +1,16 @@
+def add_number(x,y):
+    print(x + y)
+
+def sub_number(x,y):
+    print(x - y)
+
+def mul_number(x,y):
+    print(x * y)
+
+def divide_number(x,y):
+    print(x / y)
+
+add_number(10,5)
+sub_number(10,5)
+add_number(10,5)
+divide_number(10,5)
